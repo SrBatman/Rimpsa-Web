2ef064fc9167247791b2d250f8ca9345b693bc15
SrBatman <imbatmanbrahxd@gmail.com>
2024-06-20 00:21:29 -0600

    changes

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rimpsa</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/template.css') }}">
 </head>
 <body>
-<div>
+<div class="top-container">
 <section>
 
 </section>
